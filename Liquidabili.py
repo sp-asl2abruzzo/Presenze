dc766129889d6fa229e024aff1aab4ed004a4444
sp-asl2abruzzo <154312559+sp-asl2abruzzo@users.noreply.github.com>
2024-02-01 17:52:30 +0100

    Add files via upload

--- Liquidabili.py
+++ Liquidabili.py
@@ -5,7 +5,6 @@ import math
 
 def toTimedelta(time):
     if ":" in str(time):
-      print(time)
       (h,m) = time.split(':')
       d=timedelta(hours=int(h), minutes=int(m))
       return d
@@ -14,16 +13,21 @@ def toTimedelta(time):
 def tohhmm (minutes):
   hh=math.trunc(minutes/60)
   mm=int(minutes%60)
-  return str(hh)+":"+str(mm)
+  return str(hh).zfill(2)+":"+str(mm).zfill(2)
 
 
-data = pd.read_excel('StraordinariLiquidabiliComparto18-23.xlsx', sheet_name="Saldi Complessivi")
+data = pd.read_excel('StraordinariLiquidabiliComparto18-23.xlsx', sheet_name="Saldi Complessivi", nrows=2696)
 df = pd.DataFrame(data, columns=["Matricola","Nominativo","Saldo 2018","Saldo 2019","Saldo 2020","Saldo 2021","Saldo 2022","Saldo 2023"])
-
 output=[]
+
 saldi=["Saldo 2018","Saldo 2019","Saldo 2020","Saldo 2021","Saldo 2022","Saldo 2023"]
+liquidazioni=["Liquidazioni 2018","Liquidazioni 2019","Liquidazioni 2020","Liquidazioni 2021","Liquidazioni 2022","Liquidazioni 2023"]
+
+for l in liquidazioni:
+  df[l]="00:00"
 
 for index,row in df.iterrows():
+  print(index)
   hoursArray=[]
   for i in saldi:
     hoursArray.append(row[i])
@@ -32,26 +36,31 @@ for index,row in df.iterrows():
   startIndex = verifiedHoursIndexes[0]
   endIndex = verifiedHoursIndexes[len(verifiedHoursIndexes)-1]
   
-  for index in verifiedHoursIndexes:
-    print("i",index)
-    print(toTimedelta(row[saldi[index]])<=toTimedelta(row[saldi[endIndex]]),(toTimedelta(row[saldi[index]])>timedelta(hours=0,minutes=0)),(toTimedelta(row[saldi[endIndex]])>timedelta(hours=0,minutes=0)))
-    if (toTimedelta(row[saldi[index]])<=toTimedelta(row[saldi[endIndex]])) and (toTimedelta(row[saldi[index]])>timedelta(hours=0,minutes=0)) and (toTimedelta(row[saldi[endIndex]])>timedelta(hours=0,minutes=0)):
-      j=index
-      subtract=toTimedelta(row[saldi[index]])
+  for vindex in verifiedHoursIndexes:
+    # print("i",vindex)
+    # print(toTimedelta(row[saldi[vindex]])<=toTimedelta(row[saldi[endIndex]]),(toTimedelta(row[saldi[vindex]])>timedelta(hours=0,minutes=0)),(toTimedelta(row[saldi[endIndex]])>timedelta(hours=0,minutes=0)))
+    if (toTimedelta(df.at[index,saldi[vindex]])<toTimedelta(df.at[index,saldi[endIndex]])) and (toTimedelta(df.at[index,saldi[vindex]])>timedelta(hours=0,minutes=0)) and (toTimedelta(df.at[index,saldi[endIndex]])>timedelta(hours=0,minutes=0)):
+      j=vindex
+      subtract=toTimedelta(df.at[index,saldi[vindex]])
+      df.at[index,liquidazioni[j]] = tohhmm(subtract/timedelta(minutes=1))
       while j<=endIndex:
-        print("j",j)
-        row[saldi[j]]=tohhmm((toTimedelta(row[saldi[j]]) - subtract)/timedelta(minutes=1))
+        # print("j",j)
+        df.at[index,saldi[j]]=tohhmm((toTimedelta(df.at[index,saldi[j]]) - subtract)/timedelta(minutes=1))
         j=j+1
-    if (toTimedelta(row[saldi[index]])>toTimedelta(row[saldi[endIndex]])) and (toTimedelta(row[saldi[index]])>timedelta(hours=0,minutes=0)) and (toTimedelta(row[saldi[endIndex]])>timedelta(hours=0,minutes=0)):
-      j=index
-      subtract=toTimedelta(row[saldi[endIndex]])
+    if (toTimedelta(df.at[index,saldi[vindex]])>=toTimedelta(df.at[index,saldi[endIndex]])) and (toTimedelta(df.at[index,saldi[vindex]])>timedelta(hours=0,minutes=0)) and (toTimedelta(df.at[index,saldi[endIndex]])>timedelta(hours=0,minutes=0)):
+      j=vindex
+      subtract=toTimedelta(df.at[index,saldi[endIndex]])
+      df.at[index,liquidazioni[j]] = tohhmm(subtract/timedelta(minutes=1))
       while j<=endIndex:
-        row[saldi[j]]=tohhmm((toTimedelta(row[saldi[j]]) - subtract)/timedelta(minutes=1))
+        df.at[index,saldi[j]]=tohhmm((toTimedelta(df.at[index,saldi[j]]) - subtract)/timedelta(minutes=1))
         j=j+1
-  print(row,startIndex,endIndex)
+
+
+print(df)
+df.to_excel("LIQ.xlsx", index=False)
+  # print(row,startIndex,endIndex)
   
   # for s in saldi:
-
 
 
   
@@ -62,4 +71,4 @@ for index,row in df.iterrows():
   # output.append(somma/timedelta(hours=1))
 
 
-print(df)
+# print(df)
